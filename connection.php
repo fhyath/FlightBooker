c6f6a4c4fb14e0a60804d601f226da03ff739662
Abu Nadim <nadimkabir2@gmail.com>
2021-04-30 14:46:14 -0400

    created connections.php file that allows the user to run multiquery statements

---
+++ connection.php
@@ -0,0 +1,46 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "OneWay";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
+$sql = "DELETE FROM CUSTOMERS WHERE CUST_ID = '1';";
+$sql .= "INSERT INTO CUSTOMERS (CUST_ID, EMAIL, FNAME, LNAME, PWD)
+VALUES ( '1','john@example.com', 'John', 'Doe', 'JD21')";
+
+
+// if ($conn->query($sql) === TRUE) {
+//   echo "\nNew record created successfully";
+// } else {
+//   echo "Error: " . $sql . "<br>" . $conn->error;
+// }
+
+if ($conn -> multi_query($sql)) {
+    do {
+      // Store first result set
+      if ($result = $conn -> store_result()) {
+        while ($row = $result -> fetch_row()) {
+          printf("%s\n", $row[0]);
+        }
+       $result -> free_result();
+      }
+      // if there are more result-sets, the print a divider
+      if ($conn -> more_results()) {
+        printf("-------------\n");
+      }
+       //Prepare next result set
+    } while ($conn -> next_result());
+  }
+  
+
+$conn->close();
